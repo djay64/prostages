e8f4a62bc5aef5451f88a5646d00d710df806e52
Jerome Doyenard <jdoyenard001@iparla.iutbayonne.univ-pau.fr>
2019-11-19 16:19:46 +0100

    controleur codé

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route;  // permet de pouvoir faire des annotations
+use Symfony\Component\HttpFoundation\Response; // pour pouvoir utiliser reponse
 
 class ProstagesController extends AbstractController
 {
@@ -16,4 +17,44 @@ class ProstagesController extends AbstractController
             'controller_name' => 'ProstagesController',
         ]);
     }
+	
+	
+	
+	/**
+     * @Route("/", name="prostages_accueil")
+     */
+    public function index2()
+    {
+        return new Response('<html><body><h1>Bienvenue sur la page d\'accueil de Prostages</h1></body></html>') ;
+	 }
+	
+	 /**
+     * @Route("/entreprises", name="prostages_entreprises")
+     */	 
+	 public function entreprises()
+    {
+       return new Response('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>') ;
+    }
+	
+	  /**
+     * @Route("/formations", name="prostages_formations")
+     */	 
+	 public function formations()
+    {
+       return new Response('<html><body><h1>Cette page affichera la liste des formations de l\'IUT</h1></body></html>')  ;
+    }
+	
+	
+	
+
+	
+	/**
+     * @Route("/stages/{id}", name="prostages_stagesId")
+     */	 
+	 public function stages($id)
+    {
+       return new Response('Cette page affichera le descriptif du stage ayant pour identifiant $id') ;
+    }
+		
+	 
 }
