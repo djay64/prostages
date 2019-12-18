120dacf7181ee668a78e07237a2693821706b26b
Jerome Doyenard <jdoyenard001@iparla.iutbayonne.univ-pau.fr>
2019-12-18 18:26:17 +0100

    écriture du code du controleur pourrécupérer les donnéess en BD

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -6,17 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;  // permet de pouvoir faire des annotations
 use Symfony\Component\HttpFoundation\Response; // pour pouvoir utiliser reponse
 
+use App\Entity\Stage ; 
+
 class ProstagesController extends AbstractController
 {/**
      * @Route("/", name="prostages_accueil")
      */	
      public function index()
     {
-        return $this->render('prostages/index.html.twig');
+		// recuperer le repository de l'entité stage		
+		 $repositoryStage = $this->getDoctrine()->getRepository(Stage::class) ;
+		
+		// recuperer les stages enregistrés en bd
+		 $stages = $repositoryStage->findAll() ;
+		
+		
+		//envoyer les stages recupérés à la vue chargée de les afficher
+		
+		
+        return $this->render('prostages/index.html.twig',['stages'=>$stages]);
                }
+			   
+			   
 	 /**
      * @Route("/entreprises", name="prostages_entreprises")
      */	 
+	 
 	public function entreprises()
     {
 		return $this->render('prostages/entreprises.html.twig') ;
@@ -37,3 +52,4 @@ class ProstagesController extends AbstractController
 		return $this->render("prostages/stages.html.twig", ['idStage'=>$id]);
     }
 			 }
+
